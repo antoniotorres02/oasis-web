6499519995dd8b338893fcd4ea21742936ea33f6
Néstor <nestormaneiroperez@gmail.com>
2023-03-20 14:10:45 +0000

    login and register with validation basic and firebase with catch errors

--- JS/LoginValidation.js
+++ JS/LoginValidation.js
@@ -2,16 +2,26 @@ import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9
 import { auth } from './FirebaseAuth.js'
 import { showToast } from './Toastify.js'
 
-signInForm.addEventListener('submit', e => {
+signInForm.addEventListener("submit", async e => {
+    console.log("asdhashdashd")
     e.preventDefault()
 
-    const email = signInForm['email'].value;
-    const password = signInForm['password'].value;
+    const email = document.getElementById("e-mail");
+    const password = document.getElementById("pwd");
 
-    try{
-        const credentials = signInWithEmailAndPassword(auth, email, password)
-    } catch(error){
-        console.log(error)
+    try {
+        const credentials = await signInWithEmailAndPassword(auth, email.value, password.value)
+        showToast('Bienvenido:  '+email.value,'sucess')
+        console.log(credentials)
+    } catch (error) {
+        console.log("test")
+        if (error.code === "auth/wrong-password") {
+            showToast('La contraseña no coincide', 'error')
+        } else if (error.code === 'auth/user-not-found') {
+            showToast('No se encuentra el usuario', 'error')
+        } else if (error.code === 'auth/too-many-requests') {
+            showToast('Demasiados intentos, intentelo más tarde', 'error')
+        }
     }
 
-})
+});
